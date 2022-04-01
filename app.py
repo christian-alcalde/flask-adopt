5b3b8b9ce6a4fea445187ac773ad1b61f0374032
lyne cha <lynecha@berkeley.edu>
2022-04-01 15:35:52 -0700

    added validation

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
+
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -12,6 +14,7 @@ app.config['SECRET_KEY'] = "secret"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 db.create_all()
@@ -31,3 +34,32 @@ def index():
 
     return render_template('index.html', title="Adoption Agency", pets=pets)
 
+@app.route("/add", methods = ["GET", "POST"])
+def add_pet():
+    """Create add pet form and handle data adding """
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+
+        pet_name = form.pet_name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age= form.age.data
+        notes= form.notes.data
+
+        new_pet = Pet(name=pet_name,
+                    species=species,
+                    photo_url=photo_url,
+                    age=age,
+                    notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Successfully added pet: {pet_name}")
+
+        return redirect("/")
+
+    else:
+        return render_template("add-pet-form.html", form=form, title="Add Pet!")
--- forms.py
+++ forms.py
@@ -1,1 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, URLField, IntegerField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, Optional, Email, URL, AnyOf
+
+
+
+
 """Forms for adopt app."""
+
+
+
+class AddPetForm(FlaskForm):
+    """Form to add a new pet """
+
+    pet_name = StringField("Pet Name")
+    species = SelectField("Species", choices =[("c","cat"),("d","dog"),("p","porcupine")], validators=[AnyOf(values=["c","d","p"])])
+    photo_url = URLField("Photo URL", validators=[Optional(),URL()])
+    age= SelectField("Age", choices =[("baby","baby"),("young","young"),("adult","adult"),("senior","senior")], validators=[AnyOf(["baby","young","adult","senior"])])
+    notes= TextAreaField("Notes")
